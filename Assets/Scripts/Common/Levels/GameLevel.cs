cdbfedc582508fdc439462d15beeaefafba63a07
Riukko <ilan.ouamrouche@outlook.com>
2025-06-05 00:36:44 +0200

    Removal of the usage of scriptable objects for storing game levels

---
+++ Assets/Scripts/Common/Levels/GameLevel.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameLevel
+{
+    public string LevelId;
+    public string LevelName;
+    public int[,] CellTable;
+}
--- Assets/Scripts/Common/Scriptable Objects/GameLevels/GameLevelSO.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "GameLevel", menuName = "Scriptable Objects/GameLevel")]
-public class GameLevel : ScriptableObject
-{
-
-}
